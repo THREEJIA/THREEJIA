37472945b0897994fb20cea485aea229980c9c62
唐琳 <853399614@qq.com>
2016-03-21 16:11:29 +0800

    TL创建DetailTableViewCell

---
+++ GeGeJia/GeGeJia/DetailTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  DetailTableViewCell.h
+//  GeGeJia
+//
+//  Created by 孙诚 on 16/3/21.
+//  Copyright © 2016年 Tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailTableViewCell : UITableViewCell
+
+@end
