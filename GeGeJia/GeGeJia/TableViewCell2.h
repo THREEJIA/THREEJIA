f7410646838039305b13f82cf0e75316b7bd1eec
唐琳 <853399614@qq.com>
2016-03-22 10:22:46 +0800

    第二天开始

---
+++ GeGeJia/GeGeJia/TableViewCell2.h
@@ -0,0 +1,15 @@
+//
+//  TableViewCell2.h
+//  GeGeJia
+//
+//  Created by lu on 16/3/21.
+//  Copyright © 2016年 Tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell2 : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIButton *bigButton;
+- (IBAction)bigButtonWay:(UIButton *)sender;
+
+@end
---
+++ GeGeJia/GeGeJia/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GeGeJia
+//
+//  Created by 孙诚 on 16/3/21.
+//  Copyright © 2016年 Tony. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
