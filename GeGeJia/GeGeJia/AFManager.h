9d5754161afa132ffc0e17a176381ef776cfdbea
yr <yuanrui1993@yahoo.com>
2016-03-22 11:33:07 +0800

    yr3-22-2

---
+++ GeGeJia/GeGeJia/AFManager.h
@@ -0,0 +1,13 @@
+//
+//  AFManager.h
+//  GeGeJia
+//
+//  Created by Mac on 16/3/21.
+//  Copyright © 2016年 Tony. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AFManager : NSObject
+
+@end
---
+++ GeGeJia/GeGeJia/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GeGeJia
+//
+//  Created by 孙诚 on 16/3/21.
+//  Copyright © 2016年 Tony. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
